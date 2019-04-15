e42ea6463dba2425dde1631bd34f277f74fffcb2
Pierre Lambert <pierre.lam91@gmail.com>
2019-04-15 22:19:39 +0200

    ajout des case grisee et des proptypes

--- src/App.js
+++ src/App.js
@@ -1,11 +1,9 @@
 import React, { Component } from 'react';
 import Keyboard from './Keyboard'
-import Failure from './Failure'
-import Success from './Success'
 import './App.css';
+import PropTypes from 'prop-types'
 
 const words = ['whisky', 'symbole', 'voiture', 'enzyme', 'capuchon', 'badigeonner', 'linguistique', 'obsidienne'];
-
 
 class App extends Component {
   constructor(props) {
@@ -18,12 +16,14 @@ class App extends Component {
       won: false,
       word: '',
       hashWord: '',
-      spellWord: '',      
+      spellWord: '',
     }
     this.handleClick = this.handleClick.bind(this);
     this.init = this.init.bind(this)
   }
-  randomIndex(){
+
+  //création des données de la partie
+  randomIndex() {
     let random = Math.floor(Math.random() * words.length);
     return random;
   }
@@ -35,35 +35,37 @@ class App extends Component {
     return word.replace(/\w/g, '_');
   }
   spellWord(word) {
-    return word.replace(/\w/g, '_').split('');
+    return this.hashWord(word).split('');
   }
   componentDidMount() {
     this.setState({
-      random : this.randomIndex(),
+      random: this.randomIndex(),
       word: this.pickWord(this.state.random),
       hashWord: this.hashWord(this.pickWord(this.state.random)),
       spellWord: this.spellWord(this.pickWord(this.state.random))
     })
   }
 
+  //gestion des évènements suite au clic sur une lettre
   handleClick(e) {
     if (!this.state.usedLetters.includes(e.target.innerText)) {
+      e.currentTarget.classList.add('grey')
       this.setState({
         usedLetters: this.state.usedLetters.concat(e.target.innerText),
       })
+      this.verifyLetter(e.target.innerText, this.state.word, this.state.spellWord)
+      this.verifyGame(e.target.innerText);
     }
-    this.verifyLetter(e.target.innerText, this.state.word, this.state.spellWord)
-    this.verifyGame(e.target.innerText);
   }
 
   verifyLetter(letterChosen, word, foundLetters) {
     word = word.split('');
     let newCount = this.state.count;
-    word.map(function (letter, index) {
+    word.map((letter, index) => {
       if (letterChosen === letter) {
         foundLetters.splice(index, 1, letterChosen);
-        return foundLetters
       }
+      return foundLetters
     })
     if (!word.includes(letterChosen)) newCount--;
     this.setState({
@@ -79,17 +81,14 @@ class App extends Component {
         won: true,
       });
     }
-    else if (this.state.count === 0 && !this.state.word.includes(letter) ) this.setState({
+    else if (this.state.count === 0 && !this.state.word.includes(letter)) this.setState({
       fail: true,
-      count : 0,
+      count: 0,
     });
-    
+
   }
 
-  newRandom() {
-    return Math.floor(Math.random() * words.length);
-  }
-
+  //réinitialisation d'une partie
   init() {
     this.setState({
       random: this.newRandom(),
@@ -101,6 +100,10 @@ class App extends Component {
     this.newWord()
   }
 
+  newRandom() {
+    return Math.floor(Math.random() * words.length);
+  }
+
   newWord() {
     this.setState({
       word: this.pickWord(this.state.random),
@@ -109,7 +112,6 @@ class App extends Component {
     })
   }
 
-  
   render() {
     return (
       <div className="app" >
@@ -118,12 +120,31 @@ class App extends Component {
         {!this.state.fail ? this.state.won ? <Success /> : <Keyboard onClick={this.handleClick} /> : <Failure word={this.state.word} />}
         <div className="row">
           <p>Lettres déjà jouées : <br />{this.state.usedLetters.join('-')}</p>
-          {!this.state.won ? this.state.count === 0 ? <p>Dernier essai...</p> : <p>Tentatives restantes: {this.state.count}</p> : <button onClick={() => this.init()}>Rejouer ?</button>}
+          {this.state.fail && <button onClick={() => this.init()}>Rejouer ?</button>}
+          {this.state.won && <button onClick={() => this.init()}>Rejouer ?</button>}
+          {!this.state.won && !this.state.fail ? this.state.count === 0 ? <p>Dernière tentative...</p> : <p>Tentatives restantes : {this.state.count}</p> : <p></p>}
         </div>
       </div >
     )
   }
 }
 
+const Failure = ({ word }) => (
+  <div>
+    <h2>Perdu, le mot à trouver était {word}</h2>
+  </div>
+)
+const Success = () => (
+  <div>
+    <h2>Félicitations, vous avez gagné la partie!</h2>
+  </div>
+)
 
+//Declaration des proptypes
+Failure.propTypes = {
+  word : PropTypes.string.isRequired
+}
+Keyboard.propTypes = {
+  onClick : PropTypes.func.isRequired
+}
 export default App;
--- src/Success.js
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-
-const Success = () => (
-    <div>
-        <h2>Félicitations, vous avez gagné la partie!</h2>
-    </div>
-)
-
-export default Success
